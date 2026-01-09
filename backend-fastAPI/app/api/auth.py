2a3df17f359e68629744183249f46468d9a064f4
nala-hn <nalahoirunnisa7@gmail.com>
2026-01-09 12:16:39 +0700

    fix: auth

--- backend-fastAPI/app/api/auth.py
+++ backend-fastAPI/app/api/auth.py
@@ -6,6 +6,7 @@ from app.core import security
 from app.core.response import universal_response
 from pydantic import BaseModel, EmailStr
 from app.schemas.user_schema import UserCreate
+from fastapi.security import OAuth2PasswordRequestForm
 
 router = APIRouter(prefix="/auth", tags=["Authentication"])
 
@@ -14,10 +15,10 @@ class LoginRequest(BaseModel):
     password: str
 
 @router.post("/login")
-async def login(request: Request, login_data: LoginRequest, db: Session = Depends(get_db)):
-    user = user_crud.get_user_by_email(db, email=login_data.email)
+async def login(request: Request, form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    user = user_crud.get_user_by_email(db, email=form_data.username)
     
-    if not user or not security.verify_password(login_data.password, user.password_hash):
+    if not user or not security.verify_password(form_data.password, user.password_hash):
         return universal_response("Error", "Email atau password salah", str(request.url.path), 401)
     access_token = security.create_access_token(
         data={"sub": user.email, "id": str(user.id), "role": user.role}
